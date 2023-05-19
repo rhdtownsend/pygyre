e25d5f978c1a4ed2dd6de890f315ac0f3963d93f
Jarrett Rosenberg <jarrett@rosenbergz.com>
2023-05-19 12:16:41 -0500

    Add support for MESA file format version 1.20

--- source/pygyre/__init__.py
+++ source/pygyre/__init__.py
@@ -283,6 +283,27 @@ def _read_model_mesa(file):
                         'eps_nuc*eps_T',
                         'eps_nuc*eps_rho',
                         'Omega_rot']
+        elif meta['version'] == 120:
+            col_keys = ['k',
+                        'r',
+                        'M_r',
+                        'L_r',
+                        'P',
+                        'T',
+                        'rho',
+                        'nabla',
+                        'N^2',
+                        'Gamma_1',
+                        'nabla_ad',
+                        'delta',
+                        'kap',
+                        'kap kap_T',
+                        'kap kap_rho',
+                        'eps_nuc',
+                        'eps_nuc*eps_T',
+                        'eps_nuc*eps_rho',
+                        'eps_grav',
+                        'Omega_rot']
         else:
             raise ValueError("Invalid header line in file '{:s}': {:d}".format(file, meta['version']))
         
